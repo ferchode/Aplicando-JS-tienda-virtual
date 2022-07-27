eafb2677d14fa6b13945fc642b72caaacca56ff6
Fernando Yepes <yepesjuanfer7532@gmail.com>
2022-07-27 14:45:56 -0500

    Fusion navbar con menu mobile

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
+// Show & Hide Desktop Menu
+
 const btnActive = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.inactive');
 
@@ -7,5 +9,19 @@ btnActive.addEventListener('click' , toogleMenuDesktop);
 function toogleMenuDesktop (){
 
     desktopMenu.classList.toggle('inactive');
+
+}
+
+// Show & Hide Mobile Menu
+
+const burgerBtn = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+burgerBtn.addEventListener('click' , toogleMobileMenu);
+
+function toogleMobileMenu (){
+
+    mobileMenu.classList.toggle('inactive');
 
 }
